8a316a794451aa253064fdec85bfab2e1c88cc52
SubhasreeKommana <91730441+SubhasreeKommana@users.noreply.github.com>
2022-06-09 11:24:37 +0530

    Update anagram.py

--- anagram.py
+++ anagram.py
@@ -3,6 +3,7 @@ s1=input("Enter the string 1:")
 s2=input("Enter the string 2:")
 s1=s1.lower()
 s2=s2.lower()
+#compare two strings to check whether they are equal or not
 if sorted(s1)==sorted(s2):
 	print("It is an anagram")
 else:

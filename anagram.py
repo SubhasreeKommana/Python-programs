528ed90f1a96cdf3be47d01ba97b3a79231cd80b
SubhasreeKommana <91730441+SubhasreeKommana@users.noreply.github.com>
2022-06-09 10:44:25 +0530

    Add files via upload

---
+++ anagram.py
@@ -0,0 +1,9 @@
+#program to check whether the given input is anagram or not
+s1=input("Enter the string 1:")
+s2=input("Enter the string 2:")
+s1=s1.lower()
+s2=s2.lower()
+if sorted(s1)==sorted(s2):
+	print("It is an anagram")
+else:
+	print("It is not an anagram")

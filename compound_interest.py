13542269f6834882035f0861ee544fd385403720
SubhasreeKommana <91730441+SubhasreeKommana@users.noreply.github.com>
2022-06-09 11:14:24 +0530

    Add files via upload

---
+++ compound_interest.py
@@ -0,0 +1,10 @@
+#program to find compound interest using function
+def CI(P,n,r):
+	ci=P*(pow((1+r/100),n))
+	return ci
+P=float(input("Enter the Principle amount:"))
+r=float(input("Enter the rate:"))
+t=float(input("Enter the number of years:"))
+ci=CI(P,t,r)
+print("The compound interest is %.2f" %ci)
+
